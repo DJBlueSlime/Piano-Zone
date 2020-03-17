7aebfd542cf53096f0047b679432363daa325534
Josh012422 <joshua.ortiz.zuluaga@outlook.com>
2020-03-17 13:01:04 -0500

    Added SweetAlert2

---
+++ sweetAlert.js
@@ -0,0 +1,11 @@
+Swal.fire({
+  title: "Hola",
+  text: "Esta es una pagina que genera notas de piano aleatoria para que practiques"
+})
+
+Swal.fire({
+  title: "Tu nombre",
+  text: "Por favor ingresa tu nombre",
+  input: "text",
+  inputPlaceholder: "Nombre"
+})
